3bd8968cf024fef6c3751c5bad15b4a9150249d0
Trey Donovan <trey@powerhat.com>
2022-11-26 17:03:26 -0700

    added image grid, off-canvas drawer

---
+++ components/Navbar.js
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import Image from 'next/image'
+import 'tw-elements';
+
+function Navbar() {
+  return (
+    <nav className="flex justify-between items-center py-2 bg-black p-2 mt-0 fixed max-w-7xl w-full z-10 top-0">
+      <Link href="/" passHref>
+      <Image 
+      src="https://mymexicotours.s3.amazonaws.com/new_graphics/mmt-logo.png" 
+      width="500" 
+      height="43" 
+      className="text-2xl font-bold text-grey-800" 
+      alt="My Mexico Tours Logo" />
+      </Link>
+      <div className="flex">
+      <button class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg  focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out mr-1.5" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">Toggle right offcanvas</button>
+
+<div class="offcanvas offcanvas-end fixed bottom-0 flex flex-col max-w-full bg-white invisible bg-clip-padding shadow-sm outline-none transition duration-300 ease-in-out text-gray-700 top-0 right-0 border-none w-96" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+  <div class="offcanvas-header flex items-center justify-between p-4">
+    <h5 class="offcanvas-title mb-0 leading-normal font-semibold" id="offcanvasRightLabel">Offcanvas right</h5>
+    <button type="button" class="btn-close box-content w-4 h-4 p-2 -my-5 -mr-2 text-black border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body flex-grow p-4 overflow-y-auto">
+    ...
+  </div>
+</div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ['mymexicotours.s3.amazonaws.com','via.placeholder.com','d1coydiqa46lxm.cloudfront.net','v5.airtableusercontent.com','cdn.mymexicotours.com'],
+  },
+};
 
 module.exports = nextConfig
