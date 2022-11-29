4416ae6de302e66a938a7cb52076278113b8167a
Trey Donovan <trey@powerhat.com>
2022-11-29 16:29:02 -0700

    fixed tw-elements apropos https://stackoverflow.com/questions/71875270/how-to-install-the-tailwind-elements-in-nextjs

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,8 +1,10 @@
-import Link from "next/link";
-import Image from 'next/image'
+import Link from 'next/link';
+import Image from 'next/image';
 import 'tw-elements';
 
+
 function Navbar() {
+  
   return (
     <nav className="flex justify-between items-center py-2 bg-black p-2 mt-0 fixed max-w-7xl w-full z-10 top-0">
       <Link href="/" passHref>
@@ -14,14 +16,14 @@ function Navbar() {
       alt="My Mexico Tours Logo" />
       </Link>
       <div className="flex">
-      <button class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg  focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out mr-1.5" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">Toggle right offcanvas</button>
+      <button className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg  focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out mr-1.5" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">Toggle right offcanvas</button>
 
-<div class="offcanvas offcanvas-end fixed bottom-0 flex flex-col max-w-full bg-white invisible bg-clip-padding shadow-sm outline-none transition duration-300 ease-in-out text-gray-700 top-0 right-0 border-none w-96" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
-  <div class="offcanvas-header flex items-center justify-between p-4">
-    <h5 class="offcanvas-title mb-0 leading-normal font-semibold" id="offcanvasRightLabel">Offcanvas right</h5>
-    <button type="button" class="btn-close box-content w-4 h-4 p-2 -my-5 -mr-2 text-black border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+<div className="offcanvas offcanvas-end fixed bottom-0 flex flex-col max-w-full bg-black invisible bg-clip-padding shadow-sm outline-none transition duration-300 ease-in-out text-white top-0 right-0 border-none w-96" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+  <div className="offcanvas-header flex items-center justify-between p-4">
+    <h5 className="offcanvas-title mb-0 leading-normal font-semibold text-white" id="offcanvasRightLabel">Offcanvas right</h5>
+    <button type="button" className="btn-close box-content w-4 h-4 p-2 -my-5 -mr-2 text-white border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-white hover:opacity-75 hover:no-underline" data-bs-dismiss="offcanvas" aria-label="Close"></button>
   </div>
-  <div class="offcanvas-body flex-grow p-4 overflow-y-auto">
+  <div className="offcanvas-body flex-grow p-4 overflow-y-auto">
     ...
   </div>
 </div>
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import dynamic from 'next/dynamic'
 import Navbar from "../components/Navbar";
 
 export default function Home() {
+
   return (
     <div>
       <Head>
