45d6d1fb5c8578a1e3bcf9df51b7b7d9d31394ce
Trey Donovan <trey@powerhat.com>
2022-12-10 14:40:42 -0700

    tried a fix re the tw-elements loading and document error

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,19 @@
-import '../styles/globals.css'
+import { useEffect } from "react";
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }, AppProps) {
+  useEffect(() => {
+    const use = async () => {
+      (await import("tw-elements")).default;
+    };
+    use();
+  }, []);
+
   return (
     <div className="container mx-auto my-6">
-<Component {...pageProps} />
+      <Component {...pageProps} />
     </div>
   );
 }
 
-export default MyApp
+export default MyApp;
