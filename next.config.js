75dc19ad6d5c3b080b6d2823f126c2ce85f8f464
Trey Donovan <trey@powerhat.com>
2022-12-15 17:14:02 -0700

    updated image optimization settings to fix build errors

--- next.config.js
+++ next.config.js
@@ -2,8 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['mymexicotours.s3.amazonaws.com','via.placeholder.com','d1coydiqa46lxm.cloudfront.net','v5.airtableusercontent.com','cdn.mymexicotours.com'],
+    domains: [
+      "mymexicotours.s3.amazonaws.com",
+      "via.placeholder.com",
+      "d1coydiqa46lxm.cloudfront.net",
+      "v5.airtableusercontent.com",
+      "cdn.mymexicotours.com",
+    ],
+    unoptimized: true,
   },
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;
