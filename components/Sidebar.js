64597c507c00a131c4e918a2f34f6fd1389ab748
Trey Donovan <trey@powerhat.com>
2023-04-15 19:07:23 -0600

    fixed the sidebar menu system and added hamburger menu plugin

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,35 +1,15 @@
 import React from "react";
 import { useState } from "react";
+import Hamburger from "hamburger-react";
 
 export default function Sidebar() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <>
-      {!isOpen ? (
-        <button onClick={() => setIsOpen(!isOpen)}>
-          <svg
-            className="fixed z-30 flex items-center cursor-pointer right-10 top-3"
-            fill="#ffffff"
-            viewBox="0 0 100 80"
-            width="40"
-            height="40"
-          >
-            <rect width="100" height="10"></rect>
-            <rect y="30" width="100" height="10"></rect>
-            <rect y="60" width="100" height="10"></rect>
-          </svg>
-        </button>
-      ) : (
-        <button
-          className="text-2xl text-white fixed top-4 right-4 z-10"
-          onClick={() => setIsOpen(!isOpen)}
-        >
-          X
-        </button>
-      )}
+      <Hamburger toggled={isOpen} toggle={setIsOpen} color="#FFF" />
       <div
-        className={`top-0 right-0 fixed bg-blue-400 w-[35vw] h-full p-10 ${
+        className={`top-16 right-0 fixed bg-black w-[35vw] h-full p-10 ${
           isOpen ? "translate-x-0" : "translate-x-full"
         } ease-in-out duration-300`}
       >
